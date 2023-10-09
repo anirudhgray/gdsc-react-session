149b966268b8f86bcfb1b36478a07f49243d1461
anirudh <anirudh04mishra@gmail.com>
2023-10-09 10:02:51 +0530

    feat: search feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -4,6 +4,8 @@ import Post from '../components/Post';
 
 const SocialMediaFeed = () => {
   const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -15,11 +17,19 @@ const SocialMediaFeed = () => {
       });
   }, []);
 
+  // search filtering
+  useEffect(() => {
+    setFilteredPosts(posts.filter((post) =>
+      post.title.toLowerCase().includes(search.toLowerCase())
+    ))
+  }, [search, posts])
+
   return (
     <div className="max-w-3xl mx-auto my-8 px-4">
       <h1 className="text-3xl font-semibold mb-4">GDSC React Session (Social Media Feed)</h1>
+      <input placeholder='Search' className='px-4 w-full py-2 mb-4 rounded-md bg-white shadow-md' value={search} onChange={e=>setSearch(e.currentTarget.value)} />
       <div className="space-y-4">
-        {posts.map((post) => (
+        {filteredPosts.map((post) => (
           <Post id={post.id} key={post.id} userid={post.userId} title={post.title} body={post.body}  />
         ))}
       </div>
